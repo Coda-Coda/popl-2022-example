cd085050d94bca82d67117f0516e453b7b8430c3
Daniel Britten <daniel.britten@outlook.com>
2021-06-15 14:58:22 +1200

    Shift to Blockchain_Model approach. Improved specs to reflect the idea of quantifying over a snapshot state of the blockchain and then modelling from there.

--- FunctionalCorrectness.v
+++ FunctionalCorrectness.v
@@ -1,4 +1,3 @@
-(* Require Import DeepSpec.Advice. *)
 Require Import DataTypeOps.
 Require Import LayerCROWDFUNDING.
 Require Import LayerETH_layer.
@@ -32,74 +31,63 @@ Open Scope Z.
 
 Module FunctionalCorrectness.
 
-Section Block_Context. (* like bstep *)
-(* "Forall Blocks:" *)
-Context
-  (coinbase : int256)
-  (timestamp : int256)
-  (number : int256)
-  (blockhash : int256 -> int256)
-  (chainid : int256).
-
-Section Nested_Calls_Context.
-(* "Forall calls from EoA:" *)
-Context
-  (origin: addr).
-
-Section Individual_Call_Context. (* like mstep *)
-(* "Forall function calls: "*)
-Context 
-  (contract_address : addr)
-  (caller: addr)
-  (callvalue : int256)
-  (initial_balances : addr -> int256)
-  (address_always_accepts_funds : addr -> bool).
-
-  Context {HmemOps: MemoryModelOps mem}.
-  Context {memModelOps : MemoryModelOps mem}.
-  Instance GlobalLayerSpec : LayerSpecClass := {
-    memModelOps := memModelOps;
-    GetHighData := global_abstract_data_type 
-  }.
-  
-Definition initial_me := 
-  GenericMachineEnv.generic_machine_env coinbase timestamp number blockhash chainid origin contract_address caller callvalue initial_balances address_always_accepts_funds.
-
-  (*
-From the Scilla repository: https://github.com/Zilliqa/scilla-coq/blob/master/Contracts/Crowdfunding.v
-Definition balance_backed (st: cstate crowdState) : Prop :=
-  (* If the campaign not Crowdfunding_funded... *)
-  ~~ (Crowdfunding_funded (state st)) ->
-  (* the contract has enough funds to reimburse everyone. *)
-  sumn (map snd (Crowdfunding_backers (state st))) <= balance st.
+(*
+The goal here is to, in a sense, quantify over an arbitrary snapshot of the Blockchain and then model all possible interactions after that point. In particular, modelling most precisely the smart contract.
 *)
 
-Inductive ContractCall :=
- | NoOp
+Section Blockchain_Model.
+Context
+  (snapshot_timestamp : int256)
+  (snapshot_number : int256)
+  (snapshot_blockhash : int256 -> int256)
+  (snapshot_balances : addr -> int256).
+
+Context
+(address_accepts_funds : machine_env_state global_abstract_data_type -> global_abstract_data_type -> addr -> addr -> int256 -> bool).
+
+Record persistent_state := mkPersistentState {
+  ps_timestamp : int256;
+  ps_number : int256;
+  ps_balance : addr -> int256;
+  ps_blockhash : int256 -> int256
+}.
+
+Definition snapshot_ps :=
+  mkPersistentState
+    snapshot_timestamp
+    snapshot_number
+    snapshot_blockhash
+    snapshot_balances
+.
+
+Context {HmemOps: MemoryModelOps mem}.
+Context {memModelOps : MemoryModelOps mem}.
+Instance GlobalLayerSpec : LayerSpecClass := {
+  memModelOps := memModelOps;
+  GetHighData := global_abstract_data_type 
+}.
+
+Context
+  (contract_address : addr).
+
+Inductive FunctionCall :=
  | contractStep_token_transfer (a : addr) (amount : int256)
  | contractStep_crowdfunding_donate (value : int256)
  | contractStep_crowdfunding_getFunds
  | contractStep_crowdfunding_claim
 .
 
-Definition updateTimeAndBlock me_before block_count time_passing : 
-(machine_env global_abstract_data_type) :=
-(* TODO-Daniel consider using the style of update_me_balance instead. *)
-  {|  me_address := me_address me_before;
-      me_origin := me_origin me_before;
-      me_caller := me_caller me_before;
-      me_callvalue := me_callvalue me_before;
-      me_coinbase := me_coinbase me_before; 
-      me_timestamp := Int256.add time_passing (me_timestamp me_before);
-      me_number := Int256.add block_count (me_number me_before);
-      me_balance := me_balance me_before;
-      me_blockhash := me_blockhash me_before;
-      me_transfer := me_transfer me_before;
-      me_callmethod := me_callmethod me_before;
-      me_log := me_log me_before;
-      me_chainid := me_chainid me_before;
-      me_selfbalance := me_selfbalance me_before;
-|}.
+Inductive ContractCall :=
+ | NonExistentFunction (* Calling a function that is not defined for this contract. Causes a revert. *)
+ | CallFunction (f : FunctionCall) (origin caller : addr) (callvalue coinbase chainid : int256).
+
+Definition updateTimeAndBlock ps_before block_count time_passing : persistent_state :=
+mkPersistentState
+  (Int256.add time_passing (ps_timestamp ps_before))
+  (Int256.add block_count (ps_number ps_before))
+  (ps_balance ps_before)
+  (ps_blockhash ps_before)
+.
 
 Definition validTimeChange block_count time_passing current_block_number current_timestamp : bool :=
   (* Note, testing for positive block_count and time_passing is unnecessary while they are Int256 values.
@@ -113,66 +101,101 @@ Definition updateBalances sender recipient amount balances : (addr -> int256) :=
   else if Int256.eq a recipient then Int256.add (balances recipient) amount
   else (balances a).
 
-Definition extract_balances_from_me (me_current : machine_env global_abstract_data_type) : addr -> int256 := 
-   (* For the purposes of being able to retreive balances outside of a contract call init_global_abstract_data is used which *really should* have nil for ETH_successful_transfers. *) (*TODO-Daniel, that we have to do this is a little messy. *)
-  me_balance me_current init_global_abstract_data.
+Definition extract_balances_from_me (ps_current : persistent_state) : addr -> int256 := 
+  ps_balance ps_current.
 
-Definition update_me_balance me_before current_balances : machine_env global_abstract_data_type :=
-  GenericMachineEnv.generic_machine_env (me_coinbase me_before) (me_timestamp me_before) (me_number me_before) (me_blockhash me_before) (me_chainid me_before) (me_origin me_before) (me_address me_before) (me_caller me_before) (@me_callvalue global_abstract_data_type me_before) current_balances address_always_accepts_funds.
+Definition update_ps_balance ps_before latest_balances : persistent_state :=
+  mkPersistentState
+  (ps_timestamp ps_before)
+  (ps_number ps_before)
+  latest_balances
+  (ps_blockhash ps_before)
+.
 
 Definition noOverflowInTransfer (sender recipient : addr) (amount : int256) (balances : addr -> int256) : bool := 
   ((Int256.intval (balances sender)) - (Int256.intval amount) >=? 0)%Z
   && ((Int256.intval (balances recipient)) + (Int256.intval amount) <=? Int256.max_unsigned)%Z
 .
 
+Definition ps_new_balance (ps_before : persistent_state) (d : global_abstract_data_type) : persistent_state :=
+  mkPersistentState
+    (ps_timestamp ps_before)
+    (ps_number ps_before)
+    (fun a => GenericMachineEnv.current_balances contract_address (ps_balance ps_before) (ETH_successful_transfers d) a)
+    (ps_blockhash ps_before)
+.
+
+Definition extract_persistent_state (me : machine_env global_abstract_data_type) (d : global_abstract_data_type) : persistent_state :=
+  mkPersistentState
+    (me_timestamp me)
+    (me_number me)
+    (me_balance me d)
+    (me_blockhash me)
+.
 
 Definition execute_contract_call (* Note that this leaves blocknumber and timestamp UNCHANGED. Changes to these should be handled elsewhere. *)
    (call : ContractCall)
    (d_before : global_abstract_data_type)
-   (me_before : (machine_env (global_abstract_data_type)))
-   : (global_abstract_data_type * (machine_env global_abstract_data_type))
+   (ps_before : persistent_state)
+   : (global_abstract_data_type * persistent_state)
 :=
 match call with
-| NoOp => (d_before, me_before)
-| contractStep_token_transfer a amount => 
-    match runStateT (Crowdfunding_token_transfer_opt a amount me_before) d_before with
-    | Some (_, d_after) => (d_after, me_before) (* TODO fix me return value esp. balances. *)
-    | None => (d_before, me_before)
-    end
-| contractStep_crowdfunding_donate value => (d_before, me_before)
-| contractStep_crowdfunding_getFunds => (d_before, me_before)
-| contractStep_crowdfunding_claim => (d_before, me_before)
+| NonExistentFunction => (d_before, ps_before)
+| CallFunction f origin caller callvalue coinbase chainid =>
+  let me_before := GenericMachineEnv.generic_machine_env
+  coinbase
+  (ps_timestamp ps_before)
+  (ps_number ps_before)
+  (ps_blockhash ps_before)
+  chainid
+  origin 
+  contract_address
+  caller
+  callvalue
+  (ps_balance ps_before)
+  address_accepts_funds
+  in  
+  match f with
+    | contractStep_token_transfer a amount => 
+        match runStateT (Crowdfunding_token_transfer_opt a amount me_before) d_before with
+        | Some (_, d_after) => (d_after, extract_persistent_state me_before d_after)
+        | None => (d_before, ps_before)
+        end
+    | contractStep_crowdfunding_donate value => (d_before, ps_before)
+    | contractStep_crowdfunding_getFunds => (d_before, ps_before)
+    | contractStep_crowdfunding_claim => (d_before, ps_before)
+  end
 end.
 
-Inductive BlockchainAction (me_before : machine_env global_abstract_data_type) :=
+Inductive BlockchainAction (ps_before : persistent_state) :=
   | contractExecution (c : ContractCall)
   | timePassing (block_count time_passing : int256)
-                (prf : validTimeChange block_count time_passing (me_number me_before) (me_timestamp me_before) = true)
+                (prf : validTimeChange block_count time_passing (ps_number ps_before) (ps_timestamp ps_before) = true)
   | externalBalanceTransfer (sender recipient : addr) (amount : int256)
-                            (prf : sender <> contract_address /\  noOverflowInTransfer sender recipient amount (extract_balances_from_me me_before) = true)
+                            (prf : sender <> contract_address /\  noOverflowInTransfer sender recipient amount (extract_balances_from_me ps_before) = true)
   | noOp.
 
 Record StepInfo := {
   d_before_StepInfo : global_abstract_data_type;
-  me_before_StepInfo : machine_env global_abstract_data_type;
-  next_action_StepInfo : BlockchainAction me_before_StepInfo
+  ps_before_StepInfo : persistent_state;
+  next_action_StepInfo : BlockchainAction ps_before_StepInfo
 }.
 
 Definition step
   (s : StepInfo)
-  : (global_abstract_data_type * (machine_env global_abstract_data_type))
+  : (global_abstract_data_type * persistent_state)
   :=
   let action := next_action_StepInfo s in
   let d_before := d_before_StepInfo s in
-  let me_before := me_before_StepInfo s in
+  let ps_before := ps_before_StepInfo s in
   match action with
   | contractExecution c =>
-      execute_contract_call c d_before me_before
+      execute_contract_call c d_before ps_before
   | timePassing block_count time_passing prf => 
-      (d_before, updateTimeAndBlock me_before block_count time_passing)
+      (d_before, updateTimeAndBlock ps_before block_count time_passing)
   | externalBalanceTransfer sender recipient amount prf =>
-      (d_before, update_me_balance me_before (updateBalances sender recipient amount (extract_balances_from_me me_before)))
-  | noOp => (d_before, me_before)
+      (d_before, update_ps_balance ps_before (updateBalances sender recipient amount (extract_balances_from_me ps_before)))
+  | noOp => (d_before, ps_before)
   end.
 
 Definition Sequence := list StepInfo.
@@ -184,7 +207,7 @@ Fixpoint ValidSequence_helper (seq : Sequence) :=
   | [] => True
   | [s] => True
   | s1 :: ((s2 :: _) as tl) => 
-       step s1 = (d_before_StepInfo s2, me_before_StepInfo s2)
+       step s1 = (d_before_StepInfo s2, ps_before_StepInfo s2)
     /\ ValidSequence_helper tl
 end.
 
@@ -195,42 +218,38 @@ Definition ValidSequence (seq : Sequence) :=
   | [] => True
   | s :: _ => 
               d_before_StepInfo s = init_global_abstract_data
-           /\ me_before_StepInfo s = initial_me 
+           /\ ps_before_StepInfo s = snapshot_ps 
            /\ ValidSequence_helper seq
 end.
 
-Inductive ReachableState : global_abstract_data_type -> (machine_env (global_abstract_data_type)) -> Prop :=
-  | initialState : ReachableState init_global_abstract_data initial_me (* TODO switch to Crowdfunding_constructor_opt *)
+Inductive ReachableState : global_abstract_data_type -> persistent_state -> Prop :=
+  | initialState : ReachableState init_global_abstract_data snapshot_ps (* TODO switch to Crowdfunding_constructor_opt *)
   | blockchainStep : forall s1 me_after d_after,
-                      let me_before := me_before_StepInfo s1 in
+                      let me_before := ps_before_StepInfo s1 in
                       let d_before := d_before_StepInfo s1 in
                       let blockchain_action := next_action_StepInfo s1 in
                       ReachableState d_before me_before
                       ->
                       (d_after, me_after) = step {| 
-                                                   me_before_StepInfo := me_before;
+                                                   ps_before_StepInfo := me_before;
                                                    d_before_StepInfo := d_before; 
                                                    next_action_StepInfo := blockchain_action
                                                  |}
                       -> ReachableState d_after me_after.
 
-Definition ReachableState' (d_current : global_abstract_data_type) (me_current : machine_env (global_abstract_data_type)) : Prop := 
+Definition ReachableState' (d_current : global_abstract_data_type) (ps_current : persistent_state) : Prop := 
   let s :=  {| 
-              me_before_StepInfo := me_current;
+              ps_before_StepInfo := ps_current;
               d_before_StepInfo := d_current; 
-              next_action_StepInfo := noOp me_current
+              next_action_StepInfo := noOp ps_current
             |} in  
 exists (seq : Sequence) (prf : ValidSequence seq), Lists.List.In s seq.
 
-Lemma ReachableStateEquivalence : forall (d_current : global_abstract_data_type) (me_current : machine_env (global_abstract_data_type)),
-  ReachableState d_current me_current = ReachableState' d_current me_current.
+Lemma ReachableStateEquivalence : forall (d_current : global_abstract_data_type) (ps_current : persistent_state),
+  ReachableState d_current ps_current = ReachableState' d_current ps_current.
 (* TODO? *)
   Abort.
 
-End Individual_Call_Context.
-
-End Nested_Calls_Context.
-
-End Block_Context.
+End Blockchain_Model.
 
 End FunctionalCorrectness.
