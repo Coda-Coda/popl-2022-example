fb2459d03cfc6d783537a6bb0cc0154ce4c16aa5
Daniel Britten <daniel.britten@outlook.com>
2021-06-22 21:02:29 +1200

    Improvements to .nix files Best to simply run ./all from popl-2021 folder

--- shell.nix
+++ shell.nix
@@ -1,1 +1,18 @@
-(import ../../shell.nix)
+let pkgs = import (builtins.fetchTarball {
+  name = "nixos-20.09-pinned";
+  url = "https://github.com/nixos/nixpkgs/archive/5e199f944cfa599636ff93e14d27e1d08ad2d9d1.tar.gz";
+  sha256 = "15z10ql82qscv3cc2l1kvqb3s9pgip1kc500szimgvzvj0fzzbll";
+}) {}; in
+
+# Run nix-build before nix-shell, to first generate the edsger binary
+
+pkgs.mkShell {
+  buildInputs = [ 
+    (import ../../dependencies.nix)
+    ];
+
+  shellHook = ''
+    export PATH=$PATH:$PWD/../../result/binaries/built-from-src/
+  '';
+    
+}
